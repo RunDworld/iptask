7883d0c341c2204c50c2dd167d5eeed12dcc06e3
RunDworld <40021908+RunDworld@users.noreply.github.com>
2018-12-10 23:27:03 +0530

    v2.1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 8000;
-var request = require("request");
+var request = require('ajax-request');
 var getIp = require('ipware')().get_ip;
 var ip;
 
@@ -9,21 +9,28 @@ var ip;
 app.get('/',function(req,resq){
 	ip = getIp(req)
 	console.log(ip);
-	url = ""
-	request.get("http://ip-api.com/json/"+ip+"?fields=country,countryCode,region,regionName,city,zip,lat,lon,timezone",function(err,res,body){
-		console.log("1->",body);
-		resq.send(body);
-
-
+	url = "";
+	request({
+		url:"http://ip-api.com/json/"+ip+"?fields=country,countryCode,region,regionName,city,zip,lat,lon,timezone",
+		method:'get',
+		success:function(data){
+			resq.send(data);
+		}
 	});
+	// request.get("",function(err,res,body){
+	// 	console.log("1->",body);
+	// 	resq.send(res);
+
+
+	// });
 	
 	// console.log(req.headers['x-forwarded-for']);
-	console.log(req.socket.remoteAddress);
+	// console.log(req.socket.remoteAddress);
 	// res.send(req.headers['x-forwarded-for']);
 });
 
 
 
 app.listen(port,function(){
-	console.log("Listening to port + ",process.env.PORT);
+	console.log("Listening to port + ",process.env.port);
 });
