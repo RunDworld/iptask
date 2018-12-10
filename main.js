2c2bef62668bb29fdec0638e63840d19423eaab7
RunDworld <40021908+RunDworld@users.noreply.github.com>
2018-12-11 01:21:31 +0530

    v2.2

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 var express = require("express");
 var app = express();
+var requestIp = require('request-ip');
+const expressip = require('express-ip');
 var port = process.env.PORT || 8000;
 var request = require('request');
 var getIp = require('ipware')().get_ip;
+app.use(expressip().getIpInfoMiddleware);
 var ip;
 
 
 app.get('/',function(req,resq){
-	ip = getIp(req)
-	console.log(ip);
-	url = "http://api.ipstack.com/"+ip+"?access_key=9f28833f9b6e61f7d93deed7ff9941eb&fields=main";
+	// ip = getIp(req)
+	// // ip = req.socket.remoteAddress;
+	// // mp = req.connection.socket.remoteAddress;
+	// cp = req.ipInfo.ip;
+	// console.log(ip+"--"+req.ip+"---"+cp);
+	console.log(req.ip);
+	url = "http://api.ipstack.com/"+req.ip+"?access_key=9f28833f9b6e61f7d93deed7ff9941eb&fields=main";
 	request.get(url,function(err,res,body){
-		console.log("1->",res);
+		// console.log("1->",res);
 		resq.send(body);
 	});
 });
 
 
 
-app.listen(port,function(){
+app.listen(port	,function(){
 	console.log("Listening to port + ",process.env.port);
 });
