9a2ee0fe25e2210b626f04367adda28663da3f64
RunDworld <40021908+RunDworld@users.noreply.github.com>
2018-12-10 23:49:18 +0530

    v2.2.0

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 8000;
-var request = require('ajax-request');
+var request = require('request');
 var getIp = require('ipware')().get_ip;
 var ip;
 
@@ -9,28 +9,15 @@ var ip;
 app.get('/',function(req,resq){
 	ip = getIp(req)
 	console.log(ip);
-	url = "";
-	request({
-		url:"http://ip-api.com/json/"+ip+"?fields=country,countryCode,region,regionName,city,zip,lat,lon,timezone",
-		method:'get',
-		success:function(data){
-			resq.send(data);
-		}
+	url = "http://api.ipstack.com/"+ip+"?access_key=9f28833f9b6e61f7d93deed7ff9941eb&fields=main";
+	request.get(url,function(err,res,body){
+		console.log("1->",res);
+		resq.send(body);
 	});
-	// request.get("",function(err,res,body){
-	// 	console.log("1->",body);
-	// 	resq.send(res);
-
-
-	// });
-	
-	// console.log(req.headers['x-forwarded-for']);
-	// console.log(req.socket.remoteAddress);
-	// res.send(req.headers['x-forwarded-for']);
 });
 
 
 
-app.listen(port,function(){
+app.listen(3000,function(){
 	console.log("Listening to port + ",process.env.port);
 });
